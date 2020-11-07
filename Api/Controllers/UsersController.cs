ca966dafbfcb3b58ff764477920233db02e596b4
Peter Jokumsen <pjokumsen@gmail.com>
2020-11-08 00:35:58 +0200

    End of section 2

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(DataContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers() =>
+            await _context.Users.ToListAsync();
+
+        [HttpGet("{id:guid}")]
+        public async Task<AppUser> GetUser(Guid id) =>
+            await _context.Users.FindAsync(id);
+    }
+}
---
+++ Api/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Entities
+{
+    [Table("User")]
+    public class AppUser
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
