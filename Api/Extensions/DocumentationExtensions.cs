ba41a4a09f5f31bf8fe822b9af45d56b3349ada1
Peter Jokumsen <pjokumsen@gmail.com>
2020-11-08 16:16:15 +0200

    Api | Move swagger implementation into extensions

---
+++ Api/Extensions/DocumentationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Api.Extensions
+{
+    // TODO: Update to document and use authorization
+    public static class DocumentationExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Udemy Dating App API",
+                });
+
+                var dllFile = typeof(Startup).Assembly.Location;
+                var xmlPath = dllFile.Replace("dll", "xml");
+
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Udemy Dating App API V1");
+                c.RoutePrefix = "swagger";
+                c.DocExpansion(DocExpansion.None);
+            });
+
+            return app;
+        }
+    }
+}
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -5,8 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Api
 {
@@ -37,20 +35,7 @@ namespace Api
 
             services.AddCors();
             services.AddIdentityServices(_config);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Udemy Dating App API",
-                });
-
-                var dllFile = typeof(Startup).Assembly.Location;
-                var xmlPath = dllFile.Replace("dll", "xml");
-
-                c.IncludeXmlComments(xmlPath);
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,14 +46,7 @@ namespace Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Udemy Dating App API V1");
-                c.RoutePrefix = "swagger";
-                c.DocExpansion(DocExpansion.None);
-            });
-
+            app.UseSwaggerDocumentation();
             app.UseHttpsRedirection();
 
             app.UseRouting();
