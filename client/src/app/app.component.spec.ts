71155f096e48021bf41f7becd9b4f1661f377c64
Peter Jokumsen <pjokumsen@gmail.com>
2020-11-08 09:49:59 +0200

    Client | Fix app component spec

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
+import { cold } from 'jasmine-marbles';
+
 import { HttpClient } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   let httpSpy: jasmine.SpyObj<HttpClient>;
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
 
   beforeEach(async () => {
     httpSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['get']);
@@ -23,22 +27,38 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'The Udemy Dating App'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('The Udemy Dating App');
+    expect(component.title).toEqual('The Udemy Dating App');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
+  describe('after initialisation', () => {
+    beforeEach(() => {
+      httpSpy.get.and.returnValue(cold('0|', [
+        [{ id: 'id', userName: 'userName' }],
+      ]));
+
+      fixture.detectChanges();
+    });
+
+    it('should render title', () => {
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('The Udemy Dating App');
+    });
+
+    it('should show results from api', () => {
+      fixture.whenStable().then(() => {
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector('li').textContent).toContain('id - userName');
+      });
+    });
   });
 });
