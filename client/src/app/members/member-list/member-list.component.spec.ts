8e881e63787c7196a2081b0dc18b1dd5dc1cfa51
Peter Jokumsen <pjokumsen@gmail.com>
2020-12-29 23:58:26 +0200

    Client | Fix failing tests

--- client/src/app/members/member-list/member-list.component.spec.ts
+++ client/src/app/members/member-list/member-list.component.spec.ts
@@ -1,5 +1,8 @@
-import { MemberService } from 'src/app/services';
+import { cold } from 'jasmine-marbles';
+import { of } from 'rxjs';
+import { AccountService, MemberService } from 'src/app/services';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { MemberListComponent } from './member-list.component';
 
 describe('MemberListComponent', () => {
@@ -11,8 +14,12 @@ describe('MemberListComponent', () => {
     memberSvcSpy = jasmine.createSpyObj<MemberService>('MemberService', ['getMembers']);
 
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+      ],
       providers: [
         { provide: MemberService, useValue: memberSvcSpy },
+        { provide: AccountService, useValue: { currentUser$: of({}) } },
       ],
       declarations: [ MemberListComponent ]
     }).compileComponents();
@@ -21,6 +28,8 @@ describe('MemberListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MemberListComponent);
     component = fixture.componentInstance;
+    memberSvcSpy.getMembers.and.returnValue(cold('-'));
+
     fixture.detectChanges();
   });
 
